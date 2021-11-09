b96fb134212c571617d2ef2ba3b80e22d01c6ac2
svonk <example@mail.ru>
2021-11-09 16:26:36 +0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+window.addEventListener('load', function () {
+
+    let faq = document.querySelector('.faq');
+
+    faq.addEventListener('click', function (e) {
+        if (e.target.classList.contains('ask')) {
+            toggleItem(e.target);
+        }
+    });
+
+    function toggleItem(ask) {
+        let answer = ask.nextElementSibling;
+
+        if (answer.classList.contains('visible')) {
+            answer.classList.remove('visible');
+
+
+            answer.addEventListener('transitionend', function () {
+                answer.classList.remove('open');
+
+            });
+        } else {
+            answer.classList.add('visible');
+            answer.classList.add('open');
+        }
+    }
+
+});
